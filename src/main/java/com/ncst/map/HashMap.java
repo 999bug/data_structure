c4022824f9933fbdd7a4921f5f4ad9b388dcca43
李石岩 <15028999624@163.com>
2020-09-06 22:52:42 +0800

    HashMap 的基础实现

---
+++ src/main/java/com/ncst/map/HashMap.java
@@ -0,0 +1,129 @@
+package com.ncst.map;
+
+
+/**
+ * @Date 2020/9/6 22:30
+ * @Author by LiShiYan
+ * @Descaption
+ */
+public class HashMap<K, V> implements Map<K, V> {
+    private int size;
+    private Node<K, V>[] table;
+    private static final boolean RED = false;
+    private static final boolean BLACK = true;
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    public HashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        if (size == 0) {
+            return;
+        }
+        size=0;
+        for (int i = 0; i < table.length; i++) {
+            table[i]=null;
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(V value) {
+        return false;
+    }
+
+    @Override
+    public void traversal(Visitor<K, V> visitor) {
+
+    }
+
+    private static class Node<K, V> {
+        int hash;
+        K key;
+        V value;
+        boolean color = RED;
+        Node<K, V> left;
+        Node<K, V> right;
+        Node<K, V> parent;
+
+        public Node(K key, V value, Node<K, V> parent) {
+            // TODO: 2020/9/6
+            this.key = key;
+            this.value = value;
+            this.parent = parent;
+        }
+
+        /**
+         * @return 判断是否是叶子结点
+         */
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+
+        /**
+         * @return 判断度是否为2个
+         */
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
+        }
+
+        public boolean isLeftChild() {
+            return parent != null && this == parent.left;
+        }
+
+        public boolean isRightChild() {
+            return parent != null && this == parent.right;
+        }
+
+        /**
+         * @return 兄弟结点
+         */
+        public Node<K, V> sibling() {
+            if (isLeftChild()) {
+                return parent.right;
+            }
+            if (isRightChild()) {
+                return parent.left;
+            }
+            //没有兄弟结点
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node_" + key + "_" + value;
+        }
+    }
+}
